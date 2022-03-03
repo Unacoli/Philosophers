5ae68b4baf0f2f72cb349914f40370a9ecd3951f
Nao ARGOUSE <nargouse@student.42.fr>
2022-03-03 20:41:08 +0100

    Init project philosophers

---
+++ philo/include/philo.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nargouse <nargouse@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/03 20:33:14 by nargouse          #+#    #+#             */
+/*   Updated: 2022/03/03 20:39:55 by nargouse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdlib.h>
+# include <string.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <signal.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <semaphore.h>
+
+#endif
